93384a07e54d09e26ce2c0661bca39c224e43a70
mohammed ammourie <mohammed.ammourie@gmail.com>
2025-11-04 22:03:21 +0300

    a

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+router.get("/", function (req, res, next) {
+  res.send("index");
 });
-router.get('/hello', function (req, res, next) {
-    res.send('hello world');
+router.get("/hello", function (req, res, next) {
+  res.send("hello world");
 });
-router.post('/dummyStores', function (req, res, next) {
-
-
-    res.send({success: true, message: 'hello world'});
+router.post("/dummyStores", function (req, res, next) {
+  res.send({ success: true, message: "hello world" });
 });
 
 module.exports = router;
